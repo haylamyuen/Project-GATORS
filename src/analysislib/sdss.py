b331352e7df8c41beff35094ed09843ee40da869
Haylam Yuen <129972883+haylamyuen@users.noreply.github.com>
2026-04-04 07:08:09 +0000

    Massive Changes to 02_bimodality plots

--- src/analysislib/sdss.py
+++ src/analysislib/sdss.py
@@ -1,24 +1,33 @@
+# Import necessary libraries
 import pandas as pd
 
-df = pd.read_csv("SDSS_library.csv")
+# Define setup function to clean and prepare the data
+def setup(df):
+    df = df[df["zWarning"] == 0] # Set the dataframe to only include instances where zWarning was 0
 
-df = df[df["zWarning"] == 0] # Set the dataframe to only include instances where zWarning was 0
+    photometry_bands = ["u", "g", "r", "i", "z"] # Define a list of all the photometry bands
 
-photometry_bands = ["u", "g", "r", "i", "z"] # Define a list of all the photometry bands
+    # Loop through every passband and check if the value is OK
+    for band in photometry_bands:
+        df = df[df[band] > 0]
 
-# Loop through every passband and check if the value is OK
-for band in photometry_bands:
-    df = df[df[band] > 0]
+    # Separate dataframe
+    global galaxies
+    global stars
+    global qsos
+    
+    galaxies = df[df["class"] == "GALAXY"]
+    stars = df[df["class"] == "STAR"]
+    qsos = df[df["class"] == "QSO"]
 
-galaxies = df[df["class"] == "GALAXY"]
-stars = df[df["class"] == "STAR"]
-qsos = df[df["class"] == "QSO"]
+    # Remove galaxies with zero or negative values
+    galaxies = galaxies[(galaxies["petroRad_r"] > 0) & (galaxies["fracDeV_r"] > 0)]
 
-galaxies = galaxies[(galaxies["petroRad_r"] > 0) & (galaxies["fracDeV_r"] > 0)]
+    # Drop unnecessary columns
+    stars = stars.drop(columns=["petroRad_r", "petroMag_r", "fracDeV_r", "expRad_r", "deVRad_r", "expAB_r", "deVAB_r"])
+    qsos = qsos.drop(columns=["petroRad_r", "petroMag_r", "fracDeV_r", "expRad_r", "deVRad_r", "expAB_r", "deVAB_r"])
 
-stars = stars.drop(columns=["petroRad_r", "petroMag_r", "fracDeV_r", "expRad_r", "deVRad_r", "expAB_r", "deVAB_r"])
-qsos = qsos.drop(columns=["petroRad_r", "petroMag_r", "fracDeV_r", "expRad_r", "deVRad_r", "expAB_r", "deVAB_r"])
-
+# Return the three dataframes
 def _galaxies():
     return galaxies
 
